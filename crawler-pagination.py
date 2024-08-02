e39255f1f1ecb37692b41cd62d6df92f925ee292
Jake Nulty <nultinator@yahoo.com>
2024-08-02 15:38:48 -0400

    Add files via upload

---
+++ crawler-pagination.py
@@ -0,0 +1,106 @@
+import os
+import csv
+import requests
+import json
+import logging
+from urllib.parse import urlencode, urlparse
+from bs4 import BeautifulSoup
+import concurrent.futures
+from dataclasses import dataclass, field, fields, asdict
+
+API_KEY = ""
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    API_KEY = config["api_key"]
+
+## Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def scrape_search_results(keyword, location, page_number, retries=3):
+    formatted_keyword = keyword.replace(" ", "+")
+    result_number = page_number * 10
+    url = f"https://www.bing.com/search?q={formatted_keyword}&first={result_number}"
+    tries = 0
+    success = False
+    
+    while tries <= retries and not success:
+        try:
+            response = requests.get(url)
+            logger.info(f"Recieved [{response.status_code}] from: {url}")
+            if response.status_code == 200:
+                success = True
+            
+            else:
+                raise Exception(f"Failed request, Status Code {response.status_code}")
+                
+                ## Extract Data
+
+            soup = BeautifulSoup(response.text, "html.parser")            
+            headers = soup.find_all("h2")
+            
+
+            excluded_words = ["explore further"]
+            for header in headers:
+                if header.text.lower() in excluded_words:
+                    continue
+                link = header.find("a")
+                h2 = header.text
+                if not link:
+                    continue
+                href = link.get("href")
+                if "https://" not in href:
+                    href = f"https://www.bing.com{href}"
+                rank = result_number
+
+                parsed_url = urlparse(href)
+                base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+                search_data = {
+                    "name": h2,
+                    "base_url": base_url,
+                    "url": url,
+                    "page": page_number,
+                    "result_number": result_number
+                } 
+
+                print(search_data)
+                result_number += 1                
+                
+            logger.info(f"Successfully parsed data from: {url}")
+            success = True
+        
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while processing page {url}: {e}")
+            logger.info(f"Retrying request for page: {url}, retries left {retries-tries}")
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+
+
+def start_scrape(keyword, pages, location, retries=3):
+    for page in range(pages):
+        scrape_search_results(keyword, location, page, retries=retries)
+
+if __name__ == "__main__":
+
+    MAX_RETRIES = 3
+    MAX_THREADS = 5
+    PAGES = 1
+    LOCATION = "us"
+
+    logger.info(f"Crawl starting...")
+
+    ## INPUT ---> List of keywords to scrape
+    keyword_list = ["learn rust"]
+    aggregate_files = []
+
+    ## Job Processes
+    for keyword in keyword_list:
+        filename = keyword.replace(" ", "-")
+
+        start_scrape(keyword, PAGES, LOCATION, retries=MAX_RETRIES)
+        
+    logger.info(f"Crawl complete.")
